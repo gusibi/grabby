9b49857b12be5ac15d78e8128ac5ec1703d9b4ca
gusibi <czongxiao@gmail.com>
2026-05-29 08:04:29 +0800

    update site

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://grabby.eztoolab.com',
   integrations: [tailwind()],
   output: 'static',
-  adapter: cloudflare(),
 });
